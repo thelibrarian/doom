09226067bd1f5589ebb262a2e343646dcdd8182a
Tony Kemp <tony@agrav.net>
2022-11-30 19:21:06 +1100

    Upate init.el from template

--- init.el
+++ init.el
@@ -175,7 +175,7 @@
         +lsp
         +rails
         +rbenv)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
